85ebc8938b138f99dbb9900bffd1bd7ba0cfa7c7
Mohamed Medhat <mohamedmmedhat21@gmail.com>
2021-12-19 18:57:44 +0200

    improving accuracies & refactoring

--- milestone2/model4.py
+++ milestone2/model4.py
@@ -1,6 +1,6 @@
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
+from sklearn import svm
 from helper_Functions import *
 
 data, X = pre_processing()
@@ -10,10 +10,15 @@ Y = data[['VideoPopularity']].iloc[:, :]
 Y = feature_scaling(Y, 0, 100)
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=1)
 
-logreg = LogisticRegression()
-logreg.fit(X_train, y_train)
-y_pred = logreg.predict(X_test)
+C = 3
+rbf_svc = svm.SVC(kernel='rbf', gamma=0.8, C=C).fit(X, Y)
+y_pred = rbf_svc.predict(X_test)
 
 accuracy = accuracy_score(y_pred, y_test)
 print(accuracy)
-# 0.8201044036942846
+# 0.8408512916610895 C=0.1
+# 0.9267835631106947 C=0.8
+# 0.9440503279346808 C=2
+# 0.9490028108686923 C=3
+# highest accuracy because:
+# n=5 m=30,301 => n small m intermediate => use SVM with gaussian kernel
--- milestone2/model5.py
+++ milestone2/model5.py
@@ -1,6 +1,6 @@
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
+from sklearn import svm
 from helper_Functions import *
 
 data, X = pre_processing()
@@ -10,10 +10,10 @@ Y = data[['VideoPopularity']].iloc[:, :]
 Y = feature_scaling(Y, 0, 100)
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=1)
 
-logreg = LogisticRegression()
-logreg.fit(X_train, y_train)
-y_pred = logreg.predict(X_test)
+C = 3
+lin_svc = svm.LinearSVC(C=C).fit(X, Y)
+y_pred = lin_svc.predict(X_test)
 
 accuracy = accuracy_score(y_pred, y_test)
 print(accuracy)
-# 0.8201044036942846
+# 0.6547985544103868 C=3
