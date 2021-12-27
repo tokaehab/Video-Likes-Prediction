89d5925e5e8b751021f924f074a47a84cfcb48e2
Mohamed Medhat <mohamedmmedhat21@gmail.com>
2021-12-27 20:45:39 +0200

    adding testing script

--- milestone2/tuning.py
+++ milestone2/tuning.py
@@ -6,20 +6,20 @@ from milestone2.models import *
 X_train, X_test, Y_train, Y_test = pre_processing()
 
 # SVM with Polynomial kernel
-# polyC = [0.5, 0.9, 2]
-# polyD = 3
-# polyArr = []
-# for i in polyC:
-#     SVMPolykernelAccuracy, training_Time, testing_Time = SVMPolykernel(X_train, X_test, Y_train, Y_test, i, polyD)
-#     polyArr.append([i, polyD, SVMPolykernelAccuracy * 100])
-#     print('Training Time Taken by SVM with Polynomial kernel', training_Time, 'seconds')
-#     print('Testing Time Taken by SVM with Polynomial kernel', testing_Time, 'seconds')
-#     print('SVM with Polynomial kernel Accuracy', SVMPolykernelAccuracy, ' with C=', i, 'with degree=', polyD)
-#     print("-----------------------------------------------------------------------------")
-#
-# polyArr = np.array(polyArr)
-# polyDF = pd.DataFrame(polyArr, columns=['Regularization parameter(C)', 'Degree', 'Accuracy'])
-# polyDF.to_csv('PolySVM Tuning.csv')
+polyC = [0.5, 0.9, 2]
+polyD = 3
+polyArr = []
+for i in polyC:
+    SVMPolykernelAccuracy, training_Time, testing_Time = SVMPolykernel(X_train, X_test, Y_train, Y_test, i, polyD)
+    polyArr.append([i, polyD, SVMPolykernelAccuracy * 100])
+    print('Training Time Taken by SVM with Polynomial kernel', training_Time, 'seconds')
+    print('Testing Time Taken by SVM with Polynomial kernel', testing_Time, 'seconds')
+    print('SVM with Polynomial kernel Accuracy', SVMPolykernelAccuracy, ' with C=', i, 'with degree=', polyD)
+    print("-----------------------------------------------------------------------------")
+
+polyArr = np.array(polyArr)
+polyDF = pd.DataFrame(polyArr, columns=['Regularization parameter(C)', 'Degree', 'Accuracy'])
+polyDF.to_csv('PolySVM Tuning.csv')
 
 # SVM with Gaussian(RBF) kernel
 rbfC = [0.1, 0.8, 1, 3, 3, 3, 3, 3]
